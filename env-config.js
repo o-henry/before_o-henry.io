808dab3eb9ca0c1e52577ff970ffa2165e10ba8f
henry <cksgod16@gmail.com>
2021-04-16 16:00:40 +0900

    Fix env-config

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const debug = process.env.NODE_ENV !== "production";
-const name = "log";
+const name = "https://o-henry.github.io/";
 
 module.exports = {
   "process.env.BACKEND_URL": !debug ? `/${name}` : "",
