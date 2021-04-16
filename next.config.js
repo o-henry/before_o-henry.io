e5c36fdb5c7a5487576ae510a88c6f6b1d78d3bb
henry <cksgod16@gmail.com>
2021-04-16 16:55:41 +0900

    Fix env

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withMDX = require("@next/mdx")({
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "md", "mdx"],
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assertPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assertPrefix: process.env.NEXT_PUBLIC_BASE_PATH + "/",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
