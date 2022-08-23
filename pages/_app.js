c7c6727243373c00d305d4b4266b27cd3d16226e
henry <cksgod16@gmail.com>
2022-08-23 15:36:19 +0900

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
 import { MDXProvider } from "@mdx-js/react";
 import Layout from "../components/layout";
 import "../styles/main.css";
+import "../public/static/fonts/style.css";
 
 const components = {
-  code: (props) => <pre {...props} />,
-  pre: (props) => <div {...props} />,
+    code: (props) => <pre {...props} />,
+    pre: (props) => <div {...props} />,
 };
 
 export default function Nextra({ Component, pageProps }) {
-  return (
-    <MDXProvider components={components}>
-      <Layout pageTitle="Henry's log" description="Personal Blog">
-        <Component {...pageProps} />
-      </Layout>
-    </MDXProvider>
-  );
+    return (
+        <MDXProvider components={components}>
+            <Layout pageTitle="Henry's log" description="Personal Blog">
+                <Component {...pageProps} />
+            </Layout>
+        </MDXProvider>
+    );
 }
