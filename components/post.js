04897164870f77c290b275fde43e37f0d961e098
Chan Haeng Lee <48753593+o-henry@users.noreply.github.com>
2022-08-25 15:54:42 +0900

    Revert "Temp for deploy gh pages"

--- components/post.js
+++ components/post.js
@@ -1,12 +1,12 @@
 import Metadata from "./metadata";
 
 function Post({ children, meta }) {
-    return (
-        <div>
-            <Metadata meta={meta} project blog />
-            <article>{children}</article>
-        </div>
-    );
+  return (
+    <>
+      <Metadata meta={meta} project blog />
+      <article>{children}</article>
+    </>
+  );
 }
 
 export default Post;
